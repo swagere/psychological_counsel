34b7504e460aa3f0286ab197919a9a30da7a3bde
swagere <1250539180@qq.com>
2021-06-09 22:14:20 +0800

    init

---
+++ src/main/java/com/caper/psychological_counseling/PsychologicalCounselingApplication.java
@@ -0,0 +1,13 @@
+package com.caper.psychological_counseling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalCounselingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalCounselingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caper/psychological_counseling/PsychologicalCounselingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caper.psychological_counseling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalCounselingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
