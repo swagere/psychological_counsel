46f2791c34a52acc92489baa880107b850410fed
swagere <1250539180@qq.com>
2021-06-18 16:59:05 +0800

    add controller

---
+++ src/main/java/com/caper/psychological_counseling/controller/AssistantController.java
@@ -0,0 +1,10 @@
+package com.caper.psychological_counseling.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class AssistantController {
+
+}
---
+++ src/main/java/com/caper/psychological_counseling/controller/VisitController.java
@@ -0,0 +1,14 @@
+package com.caper.psychological_counseling.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author:
+ */
+
+@Slf4j
+@RestController
+public class VisitController {
+
+}
