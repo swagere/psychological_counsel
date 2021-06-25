498b726a9819a9eeb5cb4cb638af09d0596e75a7
Nancylld <nancylld553@gmail.com>
2021-06-25 11:46:28 +0800

    change the get、set and delete user into the common

--- src/main/java/com/caper/psychological_counseling/controller/CommonController.java
+++ src/main/java/com/caper/psychological_counseling/controller/CommonController.java
@@ -1,7 +1,10 @@
 package com.caper.psychological_counseling.controller;
 
+import com.caper.psychological_counseling.common.config.exception.AjaxResponse;
 import com.caper.psychological_counseling.common.config.system.SysMenuNode;
 import com.caper.psychological_counseling.common.config.system.SysMenuService;
+import com.caper.psychological_counseling.model.dto.UserDTO;
+import com.caper.psychological_counseling.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +24,56 @@ public class CommonController {
 
     @Autowired
     private SysMenuService sysMenuService;
+    @Autowired
+    private SysUserService sysUserService;
 
     @RequestMapping(value = "/tree/user/{username}", method = RequestMethod.GET)
     public List<SysMenuNode> userTree(@PathVariable("username") String username) {
         return sysMenuService.getMenuTreeByUsername(username);
+    }
+
+    //获取用户信息，根据当前ID获取
+
+    @RequestMapping(value = "/user/getUser/{id}", method =  RequestMethod.GET)
+    public AjaxResponse getUser(@PathVariable("id")Long id){
+        //System.out.println(id);
+        UserDTO userDTO = sysUserService.getSysUser(id);
+        System.out.println(userDTO);
+
+        return AjaxResponse.success(userDTO);
+
+    }
+
+
+
+    //修改用户信息
+
+
+    @PutMapping("/user/update")
+    public AjaxResponse updateUser(@RequestParam("id") Long id,
+                                   @RequestParam("telephone")Long telephone,
+                                   @RequestParam("email")String email,
+                                   @RequestParam("gender")Integer gender,
+                                   @RequestParam("description")String description){
+
+        if(id == null){
+
+            //异常
+        }
+
+        System.out.println(id);
+        sysUserService.updateSysUser(id,telephone,email,gender,description);
+
+        return AjaxResponse.success();
+    }
+
+
+    //注销账号（删除用户信息）
+
+    @DeleteMapping("/user/{id}")
+    public AjaxResponse deleteUser(@PathVariable("id")Long id){
+
+        sysUserService.deleteSysUser(id);
+        return AjaxResponse.success();
     }
 }
--- src/main/java/com/caper/psychological_counseling/controller/UserController.java
+++ src/main/java/com/caper/psychological_counseling/controller/UserController.java
@@ -40,51 +40,6 @@ public class UserController {
     private ConsultRecordService consultRecordService;
 
 
-    //获取用户信息，根据当前ID获取
-
-    @RequestMapping(value = "/user/getUser/{id}", method =  RequestMethod.GET)
-    public AjaxResponse getUser(@PathVariable("id")Long id){
-        //System.out.println(id);
-        UserDTO userDTO = sysUserService.getSysUser(id);
-        System.out.println(userDTO);
-
-        return AjaxResponse.success(userDTO);
-
-    }
-
-
-
-    //修改用户信息
-
-
-    @PutMapping("/user/update")
-    public AjaxResponse updateUser(@RequestParam("id") Long id,
-                                   @RequestParam("telephone")Long telephone,
-                                   @RequestParam("email")String email,
-                                   @RequestParam("gender")Integer gender,
-                                   @RequestParam("description")String description){
-
-        if(id == null){
-
-            //异常
-        }
-
-        System.out.println(id);
-        sysUserService.updateSysUser(id,telephone,email,gender,description);
-
-        return AjaxResponse.success();
-    }
-
-
-    //注销账号（删除用户信息）
-
-    @DeleteMapping("/user/{id}")
-    public AjaxResponse deleteUser(@PathVariable("id")Long id){
-
-        sysUserService.deleteSysUser(id);
-        return AjaxResponse.success();
-    }
-
 
     //提交初访申请（建立申请表）
 
